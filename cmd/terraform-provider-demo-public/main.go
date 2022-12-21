dd7b19d3b70dd5b28465c884edd18aa2082cb76a
pigfall <koi_town@outlook.com>
2022-12-21 14:47:58 +0900

    init

---
+++ cmd/terraform-provider-demo-public/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	tfprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+
+	"github.com/pigfall/terraform-provider-demo/pkg"
+)
+
+func main() {
+	opts := providerserver.ServeOpts{
+		Address: "registry.terraform.io/pigfall/tzzpg",
+	}
+	err := providerserver.Serve(context.Background(), func() tfprovider.Provider { return pkg.NewProvider() }, opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
---
+++ pkg/provider.go
@@ -0,0 +1,39 @@
+package pkg 
+
+import(
+	"context"
+
+  tfprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+ "github.com/hashicorp/terraform-plugin-framework/provider/schema"
+  "github.com/hashicorp/terraform-plugin-framework/datasource"
+  "github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+type Provider struct{
+
+}
+
+func NewProvider() tfprovider.Provider{
+	return &Provider{}
+}
+
+func(*Provider)	Schema(ctx context.Context, req tfprovider.SchemaRequest, resp *tfprovider.SchemaResponse){
+	resp.Schema = schema.Schema{}
+}
+
+func (p *Provider) Metadata(_ context.Context, _ tfprovider.MetadataRequest, resp *tfprovider.MetadataResponse) {
+  resp.TypeName = "demo-public"
+}
+
+func(*Provider)	Configure(context.Context, tfprovider.ConfigureRequest, *tfprovider.ConfigureResponse){
+	return
+}
+
+func(*Provider)	DataSources(context.Context) []func() datasource.DataSource{
+	return []func() datasource.DataSource{
+	}
+}
+
+func(*Provider)	Resources(context.Context) []func() resource.Resource{
+	return []func()resource.Resource{}
+}
